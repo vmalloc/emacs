df79cdefa86bb6f08f5a90b4f54ba743e10adc8e
Rotem Yaari <vmalloc@gmail.com>
2012-02-24 23:58:57 +0200

    Rename themes-legacy to legacy/themes

--- init.el
+++ init.el
@@ -46,7 +46,7 @@
   (add-to-list 'custom-theme-load-path (in-emacs-d "themes"))
   (load-theme 'vmalloc t))
  ((< emacs-major-version 24)  
-  (add-to-list 'load-path (in-emacs-d "themes-legacy/"))
+  (add-to-list 'load-path (in-emacs-d "legacy/themes/"))
   (load-library "color-theme")
   (require 'color-theme)
   (color-theme-initialize)
