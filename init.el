905dd3aace57160d4a04aa7595e70690201b542f
Rotem Yaari <vmalloc@gmail.com>
2012-03-09 23:41:25 +0200

    ghc-mod

--- init.el
+++ init.el
@@ -159,6 +159,10 @@
 (autoload 'turn-on-haskell-ghci "haskell-ghci"
   "Turn on interaction with a GHCi interpreter." t)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-ghci)
+(add-to-list 'load-path (in-modes-d "ghc-mod/elisp"))
+(autoload 'ghc-init "ghc" nil t)
+(add-hook 'haskell-mode-hook (lambda () (ghc-init) (flymake-mode)))
+(setq ghc-module-command "~/.cabal/bin/ghc-mod")
 
 ; HTML
 (autoload 'nxhtml-mode (in-modes-d "nxhtml-mode/autostart.el") nil t)
