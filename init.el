28cfbf7375ff0de7ed2082154e31a35c486fcacc
Rotem Yaari <vmalloc@gmail.com>
2012-10-09 14:27:58 +0200

    Update submodules

--- init.el
+++ init.el
@@ -316,7 +316,7 @@
 ; org-mode
 (add-to-list 'load-path (in-modes-d "org-mode/lisp"))
 (add-to-list 'load-path (in-modes-d "org-mode/contrib/lisp"))
-(require 'org-install)
+(require 'org)
 (add-hook 'org-mode-hook
           (lambda ()
             (define-key org-mode-map [(meta left)]    nil)
