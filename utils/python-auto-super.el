dd8b96a2c6a1c2e4f38938b7e267c8497501c9cc
Rotem Yaari <vmalloc@gmail.com>
2012-10-18 09:45:57 +0200

    Fix python-auto-super

--- utils/python-auto-super.el
+++ utils/python-auto-super.el
@@ -2,11 +2,11 @@
   (interactive)
   (let (methodname classname)
     (save-excursion
-      (beginning-of-defun)
+      (python-nav-beginning-of-defun)
       (or (looking-at "[ \t]*def[ \t]+\\([a-zA-Z0-9_]+\\)")
           (error "Can't determine method name"))
       (setq methodname (match-string 1))
-      (python-beginning-of-block)
+      (python-nav-backward-block)
       (or (looking-at "[ \t]*class[ \t]+\\([a-zA-Z0-9_]+\\)")
           (error "Can't determine class name"))
       (setq classname (match-string 1)))
