70ffcdc317b7e1cb81635ca62749c55e3e35977d
Rotem Yaari <vmalloc@gmail.com>
2012-11-24 22:42:46 +0200

    Add smartparens

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -4,7 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(minimap-window-location (quote right))
- '(ns-pop-up-frames nil))
+ '(ns-pop-up-frames nil)
+ '(sp-cancel-autoskip-on-backward-movement nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
