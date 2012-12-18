c36cf04f0d04b74a143c8ac84d983d1b17594fab
Rotem Yaari <vmalloc@gmail.com>
2012-12-18 22:28:05 +0200

    Smarter C-:

--- init.el
+++ init.el
@@ -368,7 +368,16 @@
 (global-set-key (kbd "C-.") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-symbol-like-this)
 (global-set-key (kbd "C-,") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-:") 'mc/mark-all-words-like-this-in-defun)
+
+(defun my/mark-all-like-this-in-defun ()
+  "wrapper for mark-all-symbols-like-this-in-defun, automatically selecting symbols if unselected"
+  (interactive)
+  (if (region-active-p)
+      (mc/mark-all-like-this-in-defun)
+    (er/mark-symbol)
+    (mc/mark-all-symbols-like-this-in-defun)))
+
+(global-set-key (kbd "C-:") 'my/mark-all-like-this-in-defun)
 (global-set-key (kbd "C-c m l") 'mc/edit-ends-of-lines)
 
 ; better compilation window
