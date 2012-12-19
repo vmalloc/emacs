b5d379e05c8e2c0594c513add6664c80f2bf0681
Rotem Yaari <vmalloc@gmail.com>
2012-12-19 10:23:28 +0200

    Saveplace: save places in files (http://whattheemacsd.com/init.el-03.html)

--- init.el
+++ init.el
@@ -84,6 +84,10 @@
 (setq recentf-auto-cleanup 'never)
 (setq recentf-max-saved-items 1000)
 
+; saveplace - save position in files (http://whattheemacsd.com/init.el-03.html)
+(require 'saveplace)
+(setq-default save-place t)
+(setq save-place-file (in-emacs-d ".places"))
 
 ; autosave settings
 (setq auto-save-list-file-prefix nil)
