4866268355db1ac5705247260b96cca29dfe260a
Rotem Yaari <vmalloc@gmail.com>
2012-12-24 13:41:38 +0200

    Tweak magit-mode

--- init.el
+++ init.el
@@ -301,8 +301,19 @@
 
 ; magit
 (add-to-list 'load-path (in-modes-d "magit"))
-(autoload 'magit-status "magit" nil t)
-(autoload 'magit-branch-manager "magit" nil t)
+(require 'magit)
+
+; make magit window full screen, restore previous frame arrangement (http://whattheemacsd.com//setup-magit.el-01.html)
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+(defun magit-quit-session ()
+  "Restores the previous window configuration and kills the magit buffer"
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
+(define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
 
 ; uniquify
 (require 'uniquify)
