69cbc66125c4a1754cd5f168e192e0309e526edd
Rotem Yaari <vmalloc@gmail.com>
2013-01-01 23:20:11 +0200

    Add rename buffer and file

--- parts/005-file-management.el
+++ parts/005-file-management.el
@@ -12,3 +12,23 @@
 (require 'saveplace)
 (setq-default save-place t)
 (setq save-place-file (in-emacs-d ".places"))
+
+; rename buffer and file
+(defun my/rename-current-buffer-file ()
+  "Renames current buffer and file it is visiting."
+  (interactive)
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (let ((new-name (read-file-name "New name: " filename)))
+        (if (get-buffer new-name)
+            (error "A buffer named '%s' already exists!" new-name)
+          (rename-file filename new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil)
+          (message "File '%s' successfully renamed to '%s'"
+                   name (file-name-nondirectory new-name)))))))
+
+(global-set-key (kbd "C-x C-r") 'my/rename-current-buffer-file)
