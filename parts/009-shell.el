ca7cde6b9adc526bc478750db71b1963232dfc87
Rotem Yaari <vmalloc@gmail.com>
2013-01-02 09:42:56 +0200

    shell-mode: C-d quits shell

---
+++ parts/009-shell.el
@@ -0,0 +1,10 @@
+(defun my/comint-delchar-or-eof-or-kill-buffer (arg)
+  (interactive "p")
+  (if (null (get-buffer-process (current-buffer)))
+      (kill-buffer)
+    (comint-delchar-or-maybe-eof arg)))
+
+(add-hook 'shell-mode-hook
+          (lambda ()
+            (define-key shell-mode-map
+              (kbd "C-d") 'my/comint-delchar-or-eof-or-kill-buffer)))
