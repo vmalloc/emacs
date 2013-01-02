b67113d0a7a81456cbfab0151f7810bcb72df437
Rotem Yaari <vmalloc@gmail.com>
2013-01-02 09:42:45 +0200

    Toggle whitespaces in magit

--- parts/023-magit-setup.el
+++ parts/023-magit-setup.el
@@ -6,9 +6,29 @@
   (window-configuration-to-register :magit-fullscreen)
   ad-do-it
   (delete-other-windows))
-(defun magit-quit-session ()
+
+(defun my/magit-quit-session ()
   "Restores the previous window configuration and kills the magit buffer"
   (interactive)
   (kill-buffer)
   (jump-to-register :magit-fullscreen))
-(define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
+(define-key magit-status-mode-map (kbd "q") 'my/magit-quit-session)
+
+(defun my/magit-toggle-whitespace ()
+  (interactive)
+  (if (member "-w" magit-diff-options)
+      (my/magit-dont-ignore-whitespace)
+    (my/magit-ignore-whitespace)))
+
+(defun my/magit-ignore-whitespace ()
+  (interactive)
+  (add-to-list 'magit-diff-options "-w")
+  (magit-refresh))
+
+(defun my/magit-dont-ignore-whitespace ()
+  (interactive)
+  (setq magit-diff-options (remove "-w" magit-diff-options))
+  (magit-refresh))
+
+(define-key magit-status-mode-map (kbd "W") 'my/magit-toggle-whitespace)
+
