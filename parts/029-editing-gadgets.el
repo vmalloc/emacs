d438b1315e61406177656e5974e63aa812f755bc
Roey Darwish Dror <roey@neocopia.com>
2013-01-02 09:40:04 +0200

    Drag-Stuff modifier keys are control+super in Linux

--- parts/029-editing-gadgets.el
+++ parts/029-editing-gadgets.el
@@ -40,7 +40,10 @@
 
 ; drag stuff
 (require-from-modes-d "drag-stuff")
-(setq drag-stuff-modifier '(meta control))
+(if (eq system-type 'gnu/linux)
+    (setq drag-stuff-modifier '(super control))
+  (setq drag-stuff-modifier '(meta control)))
+
 (drag-stuff-global-mode t)
 
 ; expand-region
