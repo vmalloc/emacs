8f822b9ff32e26db92f87a1a95708962fad9b8c0
Rotem Yaari <vmalloc@gmail.com>
2013-01-03 10:08:16 +0200

    Cleanup buffers before saving

---
+++ parts/005-buffer-management.el
@@ -0,0 +1,20 @@
+(defun my/cleanup-buffer-safe ()
+  "Perform a bunch of safe operations on the whitespace content of a buffer.
+Does not indent buffer, because it is used for a before-save-hook, and that
+might be bad."
+  (interactive)
+  (untabify (point-min) (point-max))
+  (delete-trailing-whitespace)
+  (set-buffer-file-coding-system 'utf-8))
+
+;; Various superfluous white-space. Just say no.
+(add-hook 'before-save-hook 'my/cleanup-buffer-safe)
+
+(defun my/cleanup-buffer ()
+  "Perform a bunch of operations on the whitespace content of a buffer.
+Including indent-buffer, which should not be called automatically on save."
+  (interactive)
+  (my/cleanup-buffer-safe)
+  (indent-region (point-min) (point-max)))
+
+(global-set-key (kbd "C-c n") 'my/cleanup-buffer)
