b63aaf747dcdee830dda697be1607f0ddb62daf5
Rotem Yaari <vmalloc@gmail.com>
2013-01-06 10:26:36 +0200

    ido-ubiquitous mode

--- parts/021-ido.el
+++ parts/021-ido.el
@@ -4,3 +4,19 @@
 (require 'ido-recentf-open)
 (recentf-mode 1)
 (global-set-key (kbd "C-x C-r") 'ido-recentf-open)
+
+;; Use ido everywhere
+(add-to-list 'load-path (in-modes-d "ido-ubiquitous"))
+(require 'ido-ubiquitous)
+(ido-ubiquitous-mode 1)
+
+;; Fix ido-ubiquitous for newer packages
+(defmacro ido-ubiquitous-use-new-completing-read (cmd package)
+  `(eval-after-load ,package
+     '(defadvice ,cmd (around ido-ubiquitous-new activate)
+        (let ((ido-ubiquitous-enable-compatibility nil))
+          ad-do-it))))
+
+(ido-ubiquitous-use-new-completing-read webjump 'webjump)
+(ido-ubiquitous-use-new-completing-read yas/expand 'yasnippet)
+(ido-ubiquitous-use-new-completing-read yas/visit-snippet-file 'yasnippet)
