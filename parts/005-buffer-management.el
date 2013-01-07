799d5dccaebd67586d440bfa80b946eaa8aa5ec0
Rotem Yaari <vmalloc@gmail.com>
2013-01-07 22:49:54 +0200

    Adopt rotate-windows from http://whattheemacsd.com//buffer-defuns.el-02.html

--- parts/005-buffer-management.el
+++ parts/005-buffer-management.el
@@ -20,3 +20,29 @@ Including indent-buffer, which should not be called automatically on save."
   (indent-region (point-min) (point-max)))
 
 (global-set-key (kbd "C-c n") 'my/cleanup-buffer)
+
+(defun my/rotate-windows ()
+  "Rotate your windows"
+  (interactive)
+  (cond ((not (> (count-windows)1))
+         (message "You can't rotate a single window!"))
+        (t
+         (setq i 1)
+         (setq numWindows (count-windows))
+         (while  (< i numWindows)
+           (let* (
+                  (w1 (elt (window-list) i))
+                  (w2 (elt (window-list) (+ (% i numWindows) 1)))
+
+                  (b1 (window-buffer w1))
+                  (b2 (window-buffer w2))
+
+                  (s1 (window-start w1))
+                  (s2 (window-start w2))
+                  )
+             (set-window-buffer w1  b2)
+             (set-window-buffer w2 b1)
+             (set-window-start w1 s2)
+             (set-window-start w2 s1)
+             (setq i (1+ i)))))))
+(global-set-key (kbd "M-r") 'my/rotate-windows)
