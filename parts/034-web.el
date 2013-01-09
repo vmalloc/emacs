918c6634c2fdaabe20fb91f1dba8de552c3aac90
Rotem Yaari <vmalloc@gmail.com>
2013-01-09 21:41:03 +0200

    Rebind zencoding-mode to C-c i

--- parts/034-web.el
+++ parts/034-web.el
@@ -13,3 +13,6 @@
 (setq zencoding-indentation 2)
 (add-hook 'sgml-mode-hook 'zencoding-mode)
 (add-hook 'web-mode-hook 'zencoding-mode)
+(define-key zencoding-mode-keymap (kbd "C-j") nil)
+(define-key zencoding-mode-keymap (kbd "<C-return>") nil)
+(define-key zencoding-mode-keymap (kbd "C-c i") 'zencoding-expand-line)
