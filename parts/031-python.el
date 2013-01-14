35950ec364ea64fb2fe0d5270b1c05472085504c
Rotem Yaari <vmalloc@gmail.com>
2013-01-14 21:52:11 +0200

    Emacs 23 fixes

--- parts/031-python.el
+++ parts/031-python.el
@@ -3,6 +3,7 @@
   (require-from-modes-d "python.el" 'python)))
 (add-hook 'python-mode-hook
           '(lambda ()
+             (define-key python-mode-map [(control ?c) ?l] 'pylookup-lookup)
              (local-set-key (kbd "C-c #") 'comment-or-uncomment-region)))
 
 ; pylookup
@@ -11,16 +12,22 @@
 (setq pylookup-db-file (concat pylookup-dir "/pylookup.db"))
 (add-to-list 'load-path pylookup-dir)
 (autoload 'pylookup-lookup "pylookup.el" nil t)
-(define-key python-mode-map [(control ?c) ?l] 'pylookup-lookup)
+
 
 ; run redgreen in current project
 (require 'redgreen-at-project-root)
-(define-key python-mode-map [(control ?c) ?r] 'redgreen-at-project-root)
+(add-hook 'python-mode-hook
+          '(lambda ()
+             (define-key python-mode-map [(control ?c) ?r] 'redgreen-at-project-root)))
 
 ; python-auto-super
 (require 'python-auto-super)
-(define-key python-mode-map [(control ?c) ?s] 'python-auto-super)
+(add-hook 'python-mode-hook
+          '(lambda ()
+             (define-key python-mode-map [(control ?c) ?s] 'python-auto-super)))
 
 ; python-auto-import
 (require 'python-auto-import)
-(define-key python-mode-map [(control ?c) ?i] 'python-auto-import)
+(add-hook 'python-mode-hook
+          '(lambda ()
+             (define-key python-mode-map [(control ?c) ?i] 'python-auto-import)))
