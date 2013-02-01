fce4915f09260545737815a8a2bac1cf7ac364d6
Rotem Yaari <vmalloc@gmail.com>
2013-02-01 14:22:09 +0200

    dired: M-<, M-> to behave in a reasonable way (jump to last file or first file)

--- parts/008-dired.el
+++ parts/008-dired.el
@@ -1,3 +1,21 @@
+;; shorter dired output
 (require-from-modes-d "dired-details")
 (setq-default dired-details-hidden-string "--- ")
 (dired-details-install)
+
+;; make end-of-buffer and beginning-of-buffer behave properly
+(defun dired-back-to-top ()
+  (interactive)
+  (beginning-of-buffer)
+  (next-line 2))
+
+(define-key dired-mode-map
+  (vector 'remap 'beginning-of-buffer) 'dired-back-to-top)
+
+(defun dired-jump-to-bottom ()
+  (interactive)
+  (end-of-buffer)
+  (next-line -1))
+
+(define-key dired-mode-map
+  (vector 'remap 'end-of-buffer) 'dired-jump-to-bottom)
