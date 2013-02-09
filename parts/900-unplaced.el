53f1da9d636aa2b70f8136f86e8cbec9f333e11a
Rotem Yaari <vmalloc@gmail.com>
2013-02-10 00:10:04 +0200

    Restore rainbow-mode

--- parts/900-unplaced.el
+++ parts/900-unplaced.el
@@ -1,6 +1,7 @@
-;(require 'rainbow-mode)
-;(add-to-list 'find-file-hook
-;	     (lambda () (unless (derived-mode-p 'web-mode) (rainbow-mode))))
+(add-to-list 'load-path (in-modes-d "rainbow-mode"))
+(require 'rainbow-mode)
+(add-to-list 'find-file-hook
+	     (lambda () (unless (derived-mode-p 'web-mode) (rainbow-mode))))
 
 ; nyan-mode (no .emacs.d is whole without it)
 (unless in-terminal
