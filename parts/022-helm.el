8771f60298b1be5f6acff4235b7f0834889a850e
Rotem Yaari <vmalloc@gmail.com>
2013-02-14 14:44:48 +0200

    helm-dired-mode is now removed....

--- parts/022-helm.el
+++ parts/022-helm.el
@@ -1,7 +1,6 @@
 (require 'helm)
 (require 'helm-config)
 (helm-mode t)
-(helm-dired-mode t)
 (setq helm-input-idle-delay 0)
 (global-set-key (kbd "C-c h") 'helm-mini)
 (global-set-key (kbd "M-i") 'helm-semantic-or-imenu)
