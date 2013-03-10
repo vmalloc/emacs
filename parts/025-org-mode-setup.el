54c2cfe46fd203001010b2ab374c92438f84f168
Rotem Yaari <vmalloc@gmail.com>
2013-03-10 09:33:57 +0200

    Org mode improvements

--- parts/025-org-mode-setup.el
+++ parts/025-org-mode-setup.el
@@ -7,3 +7,16 @@
             (define-key org-mode-map [(meta down)]    nil)
             (define-key org-mode-map [(meta up)]   nil))
           'append)
+
+(defun myorg-update-parent-cookie ()
+  (when (equal major-mode 'org-mode)
+    (save-excursion
+      (ignore-errors
+        (org-back-to-heading)
+        (org-update-parent-todo-statistics)))))
+
+(defadvice org-kill-line (after fix-cookies activate)
+  (myorg-update-parent-cookie))
+
+(defadvice kill-whole-line (after fix-cookies activate)
+  (myorg-update-parent-cookie))
