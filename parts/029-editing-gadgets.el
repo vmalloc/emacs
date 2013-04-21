b0bb3230d2d9067ea38829ba5bfd07a405751424
Rotem Yaari <vmalloc@gmail.com>
2013-04-21 11:30:34 +0300

    Advice undo-tree for undoing regions
    See http://whattheemacsd.com//my-misc.el-02.html

--- parts/029-editing-gadgets.el
+++ parts/029-editing-gadgets.el
@@ -12,6 +12,18 @@
 (require 'undo-tree)
 (global-undo-tree-mode t)
 
+;; Keep region when undoing in region
+(defadvice undo-tree-undo (around keep-region activate)
+  (if (use-region-p)
+      (let ((m (set-marker (make-marker) (mark)))
+            (p (set-marker (make-marker) (point))))
+        ad-do-it
+        (goto-char p)
+        (set-mark m)
+        (set-marker p nil)
+        (set-marker m nil))
+    ad-do-it))
+
 ; iedit
 (require 'iedit)
 (global-set-key (kbd "C-x i") 'iedit-mode)
