3c1d14a271bebb6db2e22249b3cea14cee28aa96
Roey Darwish Dror <roey.ghost@gmail.com>
2013-06-20 14:44:55 +0300

    Macro toggling

--- parts/029-editing-gadgets.el
+++ parts/029-editing-gadgets.el
@@ -144,3 +144,21 @@
 
 ;; Semantic
 (semantic-mode t)
+
+;; Macros
+(global-set-key '[(f1)] 'call-last-kbd-macro)
+(global-set-key '[(shift f1)] 'toggle-kbd-macro-recording-on)
+
+(defun toggle-kbd-macro-recording-on ()
+  "One-key keyboard macros: turn recording on."
+  (interactive)
+  (define-key global-map (this-command-keys)
+    'toggle-kbd-macro-recording-off)
+  (start-kbd-macro nil))
+
+(defun toggle-kbd-macro-recording-off ()
+  "One-key keyboard macros: turn recording off."
+  (interactive)
+  (define-key global-map (this-command-keys)
+    'toggle-kbd-macro-recording-on)
+  (end-kbd-macro))
