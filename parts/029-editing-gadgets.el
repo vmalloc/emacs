4eeab362d15f1a56322b7e305dda91a1d325b9c4
Rotem Yaari <vmalloc@gmail.com>
2013-08-22 17:54:36 +0300

    Better binding for multiple-cursors

--- parts/029-editing-gadgets.el
+++ parts/029-editing-gadgets.el
@@ -70,6 +70,8 @@
 (global-set-key (kbd "C-.") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-symbol-like-this)
 (global-set-key (kbd "C-,") 'mc/mark-previous-like-this)
+(global-set-key (kbd "<f6>") (lambda () (interactive) (mc/create-fake-cursor-at-point)))
+(global-set-key (kbd "S-<f6>") 'multiple-cursors-mode)
 
 (defun my/mark-all-like-this-in-defun ()
   "wrapper for mark-all-symbols-like-this-in-defun, automatically selecting symbols if unselected"
