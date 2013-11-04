106e98f1480d38695101b588f9e53401498fb775
Rotem Yaari <vmalloc@gmail.com>
2013-11-04 10:26:48 +0200

    Enable guide-key for % and * in ibuffer-mode

--- parts/003-display.el
+++ parts/003-display.el
@@ -25,5 +25,5 @@
 
 ; guid-key
 (require 'guide-key)
-(setq guide-key/guide-key-sequence '("C-x r" "C-x 4"))
-(guide-key-mode 1)  ; Enable guide-key-mode
+(setq guide-key/guide-key-sequence '("C-x r" "C-x 4" "%" "*"))
+(guide-key-mode 1)
