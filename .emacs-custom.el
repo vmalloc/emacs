c4535e2a9419ba982024ccb4217d9a65e7a7976b
Rotem Yaari <vmalloc@gmail.com>
2013-11-19 23:02:47 +0200

    disable cursor blinking

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
  '(cua-rectangle-mark-key (kbd "C-x <C-return>"))
  '(helm-ff-transformer-show-only-basename nil)
  '(minimap-window-location (quote right))
