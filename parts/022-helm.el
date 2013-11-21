0294cc36e760b4c17e845f1579e9672ca4dc44bb
Rotem Yaari <vmalloc@gmail.com>
2013-11-21 11:00:22 +0200

    Add quick directories for helm-projectile

--- parts/022-helm.el
+++ parts/022-helm.el
@@ -12,6 +12,16 @@
 
 (defun emacs-d-find-file ()
   (interactive)
-  (let ((default-directory "~/.emacs.d/"))
+  (helm-projectile-find-file-in-dir "~/.emacs.d"))
+
+(global-set-key (kbd "C-c C-e") 'emacs-d-find-file)
+
+(defun helm-projectile-find-file-in-dir (dir-name)
+  (let ((default-directory dir-name))
     (helm-projectile)))
+
+(defun make-quickdir (modifier dir-name)
+  (lexical-let ((d dir-name))
+    (let ((hook (lambda  () (interactive) (helm-projectile-find-file-in-dir d))))
+      (global-set-key modifier hook))))
 
