4a6c25958d4ed7da0255a9dc3d0eb10e55b7281a
Rotem Yaari <vmalloc@gmail.com>
2014-01-13 09:27:24 +0200

    Get rid of rogue setting overriding theme selection

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -18,7 +18,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#000000" :foreground "#dedede" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "apple" :family "Monaco"))))
  '(ace-jump-face-foreground ((t (:background "black" :foreground "yellow" :weight bold))))
  '(flycheck-error-face ((t (:background "dark red"))) t)
  '(flycheck-warning-face ((t (:background "RoyalBlue4"))) t)
