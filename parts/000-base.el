24ffd62075ac3be7ee1e08bc67db236cc0bdbd4f
Roey Darwish Dror <roey.ghost@gmail.com>
2014-01-16 13:16:15 +0200

    Make the super key work in Windows

--- parts/000-base.el
+++ parts/000-base.el
@@ -1,2 +1,11 @@
 (require 'dash)
 (require 's)
+
+;; Make the super key work in Windows
+(setq
+ w32-pass-lwindow-to-system nil
+ w32-pass-rwindow-to-system nil
+ w32-pass-apps-to-system nil
+ w32-lwindow-modifier 'super ; Left Windows key
+ w32-rwindow-modifier 'super ; Right Windows key
+ w32-apps-modifier 'hyper) ; Menu key
