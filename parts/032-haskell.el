0c94227658b3f68019d4406dddeb2f062bae9866
Roey Darwish Dror <roey.ghost@gmail.com>
2014-01-28 16:13:40 +0200

    F9 in Haskell mode launches cabal build

--- parts/032-haskell.el
+++ parts/032-haskell.el
@@ -3,8 +3,18 @@
 (autoload 'turn-on-haskell-ghci "haskell-ghci"
   "Turn on interaction with a GHCi interpreter." t)
 
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(setq haskell-process-suggest-hoogle-imports nil)
 
+(add-hook 'haskell-mode-hook
+          (lambda ()
+            (turn-on-haskell-indentation)
+            (define-key haskell-mode-map (kbd "<f9>") 'haskell-process-cabal-build)
+            ))
+
+(add-hook 'haskell-interactive-mode-hook
+          (lambda ()
+            (define-key haskell-interactive-mode-map (kbd "<f9>") 'haskell-process-cabal-build)
+            ))
 ;; (add-hook 'haskell-mode-hook 'turn-on-haskell-ghci)
 ;; (add-to-list 'load-path (in-modes-d "ghc-mod/elisp"))
 ;; (autoload 'ghc-init "ghc" nil t)
