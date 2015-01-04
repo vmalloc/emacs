70b7bc492f83c95a7976fbd9a39a211d17352e80
Rotem Yaari <vmalloc@gmail.com>
2015-01-04 09:16:58 +0200

    Remove erroneous cleanup hook

--- parts/001-defaults.el
+++ parts/001-defaults.el
@@ -54,11 +54,6 @@
 ; yes/no turns to y/n
 (fset 'yes-or-no-p 'y-or-n-p)
 
-; don't ask about running (add-hook 'after-init-hook 
-(defadvice save-buffers-kill-emacs (around no-query-kill-emacs activate)
-  "Prevent annoying \"Active processes exist\" query when you quit Emacs."
-  (flet ((process-list ())) ad-do-it))
-
 ; reasonable code offset
 (setq c-basic-offset 4)
 
