a3ab2c6a2c811ad6c4362fc204341d6d0c9c9ed6
Rotem Yaari <vmalloc@gmail.com>
2015-11-07 00:32:10 +0200

    Customization

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -3,8 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ag-executable "/usr/local/bin/ag")
  '(blink-cursor-mode nil)
  '(cua-rectangle-mark-key (kbd "C-x <C-return>"))
+ '(flycheck-display-errors-function (function flycheck-pos-tip-error-messages))
  '(helm-ff-transformer-show-only-basename nil)
  '(ido-vertical-define-keys (quote C-n-C-p-up-down-left-right))
  '(iflipb-ignore-buffers nil)
