e04b9213e469e3636d34fcdd96b89b922c5e0c83
Rotem Yaari <vmalloc@gmail.com>
2016-01-17 09:26:33 +0200

    Flycheck fixes

--- parts/999-unified.el
+++ parts/999-unified.el
@@ -44,6 +44,34 @@
 (use-package helm-ag
   :ensure t)
 
+(use-package flycheck
+  :config
+  (progn
+    (mapcar '(lambda (m) (add-hook m '(lambda () (flycheck-mode))))
+            (list 'python-mode-hook 'js-mode-hook))
+    (add-hook prog-mode-hook '(lambda () (flycheck-mode)))
+    (setq flycheck-mode-line
+      '(:eval
+        (pcase flycheck-last-status-change
+          (`not-checked nil)
+          (`no-checker (propertize " -" 'face 'warning))
+          (`running (propertize " ✷" 'face 'success))
+          (`errored (propertize " !" 'face 'error))
+          (`finished
+           (let* ((error-counts (flycheck-count-errors flycheck-current-errors))
+                  (no-errors (cdr (assq 'error error-counts)))
+                  (no-warnings (cdr (assq 'warning error-counts)))
+                  (face (cond (no-errors 'error)
+                              (no-warnings 'warning)
+                              (t 'success))))
+             (propertize (format " %s/%s" (or no-errors 0) (or no-warnings 0))
+                         'face face)))
+          (`interrupted " -")
+          (`suspicious '(propertize " ?" 'face 'warning))))))
+  :bind (([(f5)] . flycheck-previous-error)
+         ([(f6)] . flycheck-next-error)))
+
+
 ;; Defaults --------------------------------------------------------------------
 
 ;; Make the super key work in Windows
