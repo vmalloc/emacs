54818b06cf079c8bdb0f26671221176962b97b1e
Rotem Yaari <vmalloc@gmail.com>
2016-09-01 10:13:25 +0300

    magit refactoring

--- init.el
+++ init.el
@@ -509,6 +509,52 @@
 	    (setq drag-stuff-modifier '(control super)))
 	    (drag-stuff-global-mode t))
 
+;; ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+;; Source control
+;; ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+(setq magit-last-seen-setup-instructions "1.4.0")
+
+
+(defun my/magit-quit-session ()
+  "Restores the previous window configuration and kills the magit buffer"
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
+
+(defun my/magit-toggle-whitespace ()
+  (interactive)
+  (if (member "-w" magit-diff-options)
+      (my/magit-dont-ignore-whitespace)
+    (my/magit-ignore-whitespace)))
+
+(defun my/magit-ignore-whitespace ()
+  (interactive)
+  (add-to-list 'magit-diff-options "-w")
+  (magit-refresh))
+
+(defun my/magit-dont-ignore-whitespace ()
+  (interactive)
+  (setq magit-diff-options (remove "-w" magit-diff-options))
+  (magit-refresh))
+
+
+(use-package magit
+  :ensure t
+  :config
+  (progn
+    (defadvice magit-status (around magit-fullscreen activate)
+      (window-configuration-to-register :magit-fullscreen)
+      ad-do-it
+      (delete-other-windows))
+
+    (setq magit-auto-revert-mode nil)
+    (define-key magit-status-mode-map (kbd "q") 'my/magit-quit-session)
+    (define-key magit-status-mode-map (kbd "W") 'my/magit-toggle-whitespace)
+    (add-hook 'git-commit-setup-hook 'git-commit-turn-on-flyspell)
+    (add-hook 'magit-mode-hook 'magit-load-config-extensions))
+
+  :bind (("C-c g" . magit-status)))
+
 
 ;; ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ;; Misc Development features
@@ -726,6 +772,7 @@ If point was already at that position, move point to beginning of line."
 (use-package
   restclient
   :ensure t)
+
 
 
 ;;; init.el ends here
