a8187c347a91087a9bd31cb17dcdff89ee6b8d2c
Rotem Yaari <vmalloc@gmail.com>
2017-02-20 13:58:27 +0200

    Add missing require for expand-region

--- custom/vmalloc-multiple-cursors-utils.el
+++ custom/vmalloc-multiple-cursors-utils.el
@@ -1,6 +1,7 @@
 (defun vmalloc/mark-all-symbols-like-this-in-defun ()
   "wrapper for mark-all-symbols-like-this-in-defun, automatically selecting symbols if unselected"
   (interactive)
+  (require 'er-basic-expansions)
   (if (not (region-active-p))
       (er/mark-symbol))
   (mc/mark-all-symbols-like-this-in-defun))
