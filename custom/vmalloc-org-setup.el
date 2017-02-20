818b9a44e9e5e10296724753cefd3a6e397f8ed2
Rotem Yaari <vmalloc@gmail.com>
2017-02-20 15:21:07 +0200

    Org setup

--- custom/vmalloc-org-setup.el
+++ custom/vmalloc-org-setup.el
@@ -1,8 +1,16 @@
+(setq org-directory "~/Dropbox/orgs")
+
+(setq org-default-notes-file (concat org-directory "/todo.org"))
+
+(setq org-agenda-files (list org-directory))
+
+
 (setq org-todo-keywords
       '((sequence "TODO" "PENDING" "NEXT" "DOING" "|" "VERIFY" "DONE" "DELEGATED")))
 
 (setq org-todo-keyword-faces
       '(("DOING" . "yellow")
+        ("NEXT" . "orange3")
         ("PENDING" . "gray")))
 
 (setq org-priority-faces '((?A . (:foreground "OrangeRed3" :weight 'bold))
@@ -18,7 +26,10 @@
         ("l" "Toolbox Link" entry (file+headline "toolbox.org" "Links")
          "* [[%c][%^{Title for link}]]       :toolbox:link:
 %?
-")))
+")
+        ("m" "Merge request" entry (file+headline "todo.org" "Merge Requests")
+          "* PENDING Merge request %c")
+        ))
 
 (setq org-refile-targets
       '(("todo.org" :maxlevel . 1)
